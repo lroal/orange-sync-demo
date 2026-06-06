d0b5e82e4cfcca6f95aa40b8f429332d8010fa1f
Lars-Erik Roald <lars.roald@systor.st>
2026-06-06 13:01:26 +0200

    Initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,16 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 5173,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@sqlite.org/sqlite-wasm']
+  }
+});
